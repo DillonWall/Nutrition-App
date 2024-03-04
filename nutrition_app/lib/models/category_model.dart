764d614ccf6359c033d9076c1f3cdf44fa315cf5
Dillon Wall <dillonmwall@msn.com>
2024-03-04 18:55:16 +0800

    Remove unused imports

--- nutrition_app/lib/models/category_model.dart
+++ nutrition_app/lib/models/category_model.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class CategoryModel {
   String name;
   String iconPath;
--- nutrition_app/lib/models/diet_model.dart
+++ nutrition_app/lib/models/diet_model.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class DietModel {
   String name;
   String iconPath;
